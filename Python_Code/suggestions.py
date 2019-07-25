0f37d4baef6ae40d6bb5d91dda17cb5e5d9378fb
zerbeln <nick.zerbel@gmail.com>
2019-07-25 15:32:23 -0700

    new suggestion added

--- Python_Code/suggestions.py
+++ Python_Code/suggestions.py
@@ -26,7 +26,20 @@ def low_high_split(nobservers, rover_dist, rover_id, poi_id, poi_values):
 
     return partners, npartners
 
-def satisfy_coupling_high_value(rover_dist, poi_id, poi_values):
+def satisfy_coupling_high_value_incentive(rover_dist, poi_id, poi_values):
+    npartners = p.coupling - 1
+
+    partners = np.zeros(npartners)
+    if poi_values[poi_id] > 5.0:
+        for partner_id in range(npartners):
+            partners[partner_id] = p.min_distance
+    else:
+        for partner_id in range(npartners):
+            partners[partner_id] = rover_dist
+
+    return partners, npartners
+
+def high_value_only(rover_dist, poi_id, poi_values):
     npartners = p.coupling - 1
 
     partners = np.zeros(npartners)
@@ -36,23 +49,6 @@ def satisfy_coupling_high_value(rover_dist, poi_id, poi_values):
     else:
         for partner_id in range(npartners):
             partners[partner_id] = 100.0
-
-    return partners, npartners
-
-def high_value_pois(nobservers, rover_dist, poi_id, poi_values):
-    npartners = p.coupling - nobservers
-
-    if npartners > 0:
-        partners = np.zeros(npartners)
-        if poi_values[poi_id] > 5.0:
-            for partner_id in range(npartners):
-                partners[partner_id] = rover_dist
-        else:
-            for partner_id in range(npartners):
-                partners[partner_id] = 100.0
-    else:
-        partners = 0
-        npartners = 0
 
     return partners, npartners
 
@@ -88,5 +84,19 @@ def position_based(nobservers, rover_dist, rx, ry, poi_id, poi_values):
     else:
         partners = 0
         npartners = 0
+
+    return partners, npartners
+
+def negative_distances(rover_dist, poi_id, poi_values):
+    npartners = p.coupling - 1
+
+    partners = np.zeros(npartners)
+
+    if poi_values[poi_id] > 5:
+        for partner_id in range(npartners):
+            partners[partner_id] = rover_dist
+    else:
+        for partner_id in range(npartners):
+            partners[partner_id] = -10.0
 
     return partners, npartners
